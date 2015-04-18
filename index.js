4fef6c50e2cdb4d579606f26ef8ed25f963984b3
Tim Finley <tfinley@hubspot.com>
2015-04-17 23:41:32 -0400

    Convert to a no-op filter, that never writes anything to disk.
    So it only calls processFile (and processString) when a file is changed and returns the srcDir from read so (eliminating the need for any output directory).

--- index.js
+++ index.js
@@ -11,14 +11,13 @@ var mapSeries = require('promise-map-series')
 module.exports = Filter
 function Filter (inputTree, options) {
   if (!inputTree) {
-    throw new Error('broccoli-filter must be passed an inputTree, instead it received `undefined`');
+    throw new Error('broccoli-noop-filter must be passed an inputTree, instead it received `undefined`');
   }
   this.inputTree = inputTree
   options = options || {}
   if (options.extensions != null) this.extensions = options.extensions
   if (options.targetExtension != null) this.targetExtension = options.targetExtension
   if (options.inputEncoding !== undefined) this.inputEncoding = options.inputEncoding
-  if (options.outputEncoding !== undefined) this.outputEncoding = options.outputEncoding
 }
 
 Filter.prototype.rebuild = function () {
@@ -26,14 +25,9 @@ Filter.prototype.rebuild = function () {
 
   var paths = walkSync(this.inputPath)
     return mapSeries(paths, function (relativePath) {
-      if (relativePath.slice(-1) === '/') {
-        mkdirp.sync(self.outputPath + '/' + relativePath)
-      } else {
+      if (relativePath.slice(-1) !== '/') {
         if (self.canProcessFile(relativePath)) {
-          return self.processAndCacheFile(self.inputPath, self.outputPath, relativePath)
-        } else {
-          helpers.copyPreserveSync(
-            self.inputPath + '/' + relativePath, self.outputPath + '/' + relativePath)
+          return self.processAndCacheFile(self.inputPath, relativePath)
         }
       }
     })
@@ -44,8 +38,6 @@ Filter.prototype.rebuild = function () {
 Filter.prototype.read = function (readTree) {
   var self = this
 
-  quickTemp.makeOrRemake(this, 'outputPath')
-  quickTemp.makeOrReuse(this, 'cachePath')
   this.needsCleanup = true
 
   return readTree(this.inputTree)
@@ -54,16 +46,11 @@ Filter.prototype.read = function (readTree) {
       return self.rebuild()
     })
     .then(function () {
-      return self.outputPath
+      return self.inputPath
     })
 }
 
-Filter.prototype.cleanup = function () {
-  if (this.needsCleanup) {
-    quickTemp.remove(this, 'outputPath')
-    quickTemp.remove(this, 'cachePath')
-  }
-}
+Filter.prototype.cleanup = function () {}
 
 Filter.prototype.canProcessFile = function (relativePath) {
   return this.getDestFilePath(relativePath) != null
@@ -85,18 +72,18 @@ Filter.prototype.getDestFilePath = function (relativePath) {
 // To do: Get rid of the srcDir/destDir args because we now have inputPath/outputPath
 // https://github.com/search?q=processAndCacheFile&type=Code&utf8=%E2%9C%93
 
-Filter.prototype.processAndCacheFile = function (srcDir, destDir, relativePath) {
+Filter.prototype.processAndCacheFile = function (srcDir, relativePath) {
   var self = this
 
   this._cache = this._cache || {}
   this._cacheIndex = this._cacheIndex || 0
   var cacheEntry = this._cache[relativePath]
   if (cacheEntry != null && cacheEntry.hash === hash(cacheEntry.inputFiles)) {
-    copyFromCache(cacheEntry)
+    // Do nothing
   } else {
     return Promise.resolve()
       .then(function () {
-        return self.processFile(srcDir, destDir, relativePath)
+        return self.processFile(srcDir, relativePath)
       })
       .catch(function (err) {
         // Augment for helpful error reporting
@@ -107,9 +94,6 @@ Filter.prototype.processAndCacheFile = function (srcDir, destDir, relativePath) 
         if (err.column != null) err.broccoliInfo.firstColumn = err.column
         throw err
       })
-      .then(function (cacheInfo) {
-        copyToCache(cacheInfo)
-      })
   }
 
   function hash (filePaths) {
@@ -117,45 +101,11 @@ Filter.prototype.processAndCacheFile = function (srcDir, destDir, relativePath) 
       return helpers.hashTree(srcDir + '/' + filePath)
     }).join(',')
   }
-
-  function copyFromCache (cacheEntry) {
-    for (var i = 0; i < cacheEntry.outputFiles.length; i++) {
-      var dest = destDir + '/' + cacheEntry.outputFiles[i]
-      mkdirp.sync(path.dirname(dest))
-      // We may be able to link as an optimization here, because we control
-      // the cache directory; we need to be 100% sure though that we don't try
-      // to hardlink symlinks, as that can lead to directory hardlinks on OS X
-      helpers.copyPreserveSync(
-        self.cachePath + '/' + cacheEntry.cacheFiles[i], dest)
-    }
-  }
-
-  function copyToCache (cacheInfo) {
-    var cacheEntry = {
-      inputFiles: (cacheInfo || {}).inputFiles || [relativePath],
-      outputFiles: (cacheInfo || {}).outputFiles || [self.getDestFilePath(relativePath)],
-      cacheFiles: []
-    }
-    for (var i = 0; i < cacheEntry.outputFiles.length; i++) {
-      var cacheFile = (self._cacheIndex++) + ''
-      cacheEntry.cacheFiles.push(cacheFile)
-      helpers.copyPreserveSync(
-        destDir + '/' + cacheEntry.outputFiles[i],
-        self.cachePath + '/' + cacheFile)
-    }
-    cacheEntry.hash = hash(cacheEntry.inputFiles)
-    self._cache[relativePath] = cacheEntry
-  }
 }
 
-Filter.prototype.processFile = function (srcDir, destDir, relativePath) {
+Filter.prototype.processFile = function (srcDir, relativePath) {
   var self = this
   var inputEncoding = (this.inputEncoding === undefined) ? 'utf8' : this.inputEncoding
-  var outputEncoding = (this.outputEncoding === undefined) ? 'utf8' : this.outputEncoding
   var string = fs.readFileSync(srcDir + '/' + relativePath, { encoding: inputEncoding })
   return Promise.resolve(self.processString(string, relativePath))
-    .then(function (outputString) {
-      var outputPath = self.getDestFilePath(relativePath)
-      fs.writeFileSync(destDir + '/' + outputPath, outputString, { encoding: outputEncoding })
-    })
 }
